cf796f22616dc333d4fcf27fdd53e54c359c324a
Dinesh Kumar G <gdineshda@gmail.com>
2024-03-15 21:13:34 +0530

    Make VAO usable

--- HelloTriangle/src/vao.h
+++ HelloTriangle/src/vao.h
@@ -8,9 +8,30 @@ typedef struct {
 	GLuint id;
 } VAO;
 
+
+static inline void vao_init(VAO *v);
+static inline void vao_bind(VAO *v);
+static inline void vao_unbind(VAO *v);
+static inline void vao_destroy(VAO *v);
+
 #define GLDE_VAO_IMPL // Debug
 #ifdef GLDE_VAO_IMPL
 
+static inline void vao_init(VAO *v) {
+	glGenVertexArrays(1, &v -> id);
+}
+
+static inline void vao_bind(VAO *v) {
+	glBindVertexArray(v -> id);
+}
+
+static inline void vao_unbind(VAO *v) {
+	glBindVertexArray(0);
+}
+
+static inline void vao_destroy(VAO *v) {
+	glDeleteVertexArrays(1, &v -> id);
+}
+
 #endif // GLDE_VAO_IMPL
 #endif // !GLDE_VAO_H
-
